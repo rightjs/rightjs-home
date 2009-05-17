a237318530ec862ff5562c949cf784b28f97f70b
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-05-17 09:43:21 +0400

    Home page filling up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ ActionController::Routing::Routes.draw do |map|
   
   map.with_options :controller => 'pages' do |pages|
     pages.with_options :action => 'show' do |show|
+      show.documentation 'docs', :requirements => {:path => ['docs']}
+      show.download 'download', :requirements => {:path => ['download']}
+      show.contacts 'contacts', :requirements => {:path => ['contacts']}
+      
       show.pages '*path'
     end
   end
