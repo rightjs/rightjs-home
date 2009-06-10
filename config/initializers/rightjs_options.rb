a002d2296dc3aea56f7f751079386eda15578ff7
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-06-10 14:00:25 +0400

    Link to the tickets tracker was added

--- config/initializers/rightjs_options.rb
+++ config/initializers/rightjs_options.rb
@@ -7,3 +7,6 @@ RIGHTJS_BUILD         = "#{RAILS_ROOT}/public/builds/current"
 RIGHTJS_BUILD_CUSTOM  = RIGHTJS_BUILD + "/custom"
 
 RIGHTJS_BUILD_OPTIONS = %w(cookie form xhr fx build)
+
+
+RIGHTJS_TICKETS_TRACKER_URL = 'http://rightjs.lighthouseapp.com/projects/31989'
