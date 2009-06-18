c92a709025ec6dc81e35d7ca5f0f895700b580d6
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-06-18 10:33:02 +0400

    preprelease clean up

--- config/initializers/rightjs_options.rb
+++ config/initializers/rightjs_options.rb
@@ -2,6 +2,8 @@
 # Here are the global site level options
 #
 
+RIGHTJS_VERSION       = "1.0.0"
+
 RIGHTJS_ROOT          = "#{RAILS_ROOT}/lib/right_js"
 RIGHTJS_BUILD         = "#{RAILS_ROOT}/public/builds/current"
 RIGHTJS_BUILD_CUSTOM  = RIGHTJS_BUILD + "/custom"
