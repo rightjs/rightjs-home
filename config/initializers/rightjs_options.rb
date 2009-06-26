d0ad4081522dcc76f232a0d67ab96e60d0221100
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-06-26 20:02:05 +0400

    Version and visual effects update

--- config/initializers/rightjs_options.rb
+++ config/initializers/rightjs_options.rb
@@ -2,7 +2,7 @@
 # Here are the global site level options
 #
 
-RIGHTJS_VERSION       = "1.0.2"
+RIGHTJS_VERSION       = "1.0.4"
 
 RIGHTJS_ROOT          = "#{RAILS_ROOT}/lib/right_js"
 RIGHTJS_BUILD         = "#{RAILS_ROOT}/public/builds/current"
