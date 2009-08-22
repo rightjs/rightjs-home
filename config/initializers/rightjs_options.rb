6e9316da0ceb3b3cba79e0c2984ba9e3a004109a
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-08-22 16:30:00 +0400

    RightJS 1.4.1 update

--- config/initializers/rightjs_options.rb
+++ config/initializers/rightjs_options.rb
@@ -2,7 +2,7 @@
 # Here are the global site level options
 #
 
-RIGHTJS_VERSION       = "1.4.0"
+RIGHTJS_VERSION       = "1.4.1"
 
 RIGHTJS_ROOT          = "#{RAILS_ROOT}/lib/right_js"
 RIGHTJS_BUILD         = "#{RAILS_ROOT}/public/builds/current"
