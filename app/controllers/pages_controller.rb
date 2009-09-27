d02f3575300d94e3940f6925d5437383f4752f4e
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-09-27 19:48:53 +0400

    Sortables documentation and demo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,15 @@
 class PagesController < ApplicationController
   caches_page :show
   
+  skip_before_filter :verify_authenticity_token
+  
   def show
     @path = params[:path].blank? ? 'home' : params[:path].join('/')
     
-    if @path.ends_with?('.js')
+    if @path.ends_with?('move.html')
+      # used in the sortable demo
+      render :text => sortable_move_resopnse, :layout => false
+    elsif @path.ends_with?('.js')
       # used in the autocompleter demo
       render :text => language_autocompleter_response, :layout => false
     else
@@ -17,6 +22,14 @@ class PagesController < ApplicationController
   end
   
 private
+  #
+  def sortable_move_resopnse
+    %{
+      <script type="text/javascript">
+        $('moving-status').update("You moved <b>Item ##{params[:path][2]}</b> to the <b>#{(params[:position].to_i + 1)}</b> position");
+      </script>
+    }
+  end
 
   # the autocompelter demo response compiller
   def language_autocompleter_response
