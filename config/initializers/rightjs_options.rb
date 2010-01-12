aca2b897838a7ee0822010afc1450644935914f7
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2010-01-12 17:19:20 +0300

    Version 1.5.3 changes

--- config/initializers/rightjs_options.rb
+++ config/initializers/rightjs_options.rb
@@ -2,7 +2,7 @@
 # Here are the global site level options
 #
 
-RIGHTJS_VERSION       = "1.5.2"
+RIGHTJS_VERSION       = "1.5.3"
 
 RIGHTJS_ROOT          = "#{RAILS_ROOT}/vendor/right_js"
 RIGHTJS_BUILD         = "#{RAILS_ROOT}/public/builds"
