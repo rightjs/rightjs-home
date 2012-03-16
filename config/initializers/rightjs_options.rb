184eb2ca8708429d7cdc7a88d02cefa72d39bcc8
Nikolay Nemshilov <nemshilov@gmail.com>
2012-03-16 10:47:27 +0400

    Bumping up the RightJS version number

--- config/initializers/rightjs_options.rb
+++ config/initializers/rightjs_options.rb
@@ -2,7 +2,7 @@
 # Here are the global site level options
 #
 
-RIGHTJS_VERSION       = "2.3.0"
+RIGHTJS_VERSION       = "2.3.1"
 
 RIGHTJS_ROOT          = "#{RAILS_ROOT}/vendor/right_js"
 RIGHTJS_BUILD         = "#{RAILS_ROOT}/public/builds"
